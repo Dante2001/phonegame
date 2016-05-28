49e0a5cc5fcd904cabee2951bb940f439adb5e14
Dante2001 <andrianodia@hotmail.com>
2016-05-28 15:44:18 -0300

    first collision

--- Assets/Scripts/PhoneHolder.cs
+++ Assets/Scripts/PhoneHolder.cs
@@ -7,7 +7,28 @@ public class PhoneHolder : MonoBehaviour {
 	void Start () {
 	
 	}
-	
+
+	void OnTriggerEnter(Collider col) {
+		if (col.gameObject.tag.Equals ("Monster")) {
+			float force = 300;
+			float xForce;
+			float zForce;
+			if (col.transform.position.x > this.transform.position.x) {
+				xForce = -force;
+			} else {
+				xForce = force;
+			}
+			if (col.transform.position.z > this.transform.position.z) {
+				zForce = -force;
+			} else {
+				zForce = force;
+			}
+
+			GetComponent<Rigidbody> ().AddForce (xForce, 0, zForce);
+			//GetComponent<Rigidbody> ().AddForceAtPosition(new Vector3(300, 0, 300),col.gameObject.transform.position);
+		}
+	}
+
 	// Update is called once per frame
 	void Update () {
 	
