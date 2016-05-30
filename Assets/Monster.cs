26ba48ffead481eb7e4ce41af30343c6d6ae16ba
Dante2001 <andrianodia@hotmail.com>
2016-05-30 20:56:22 -0300

    fixed stuff

--- Assets/Monster.cs
+++ Assets/Monster.cs
@@ -7,13 +7,9 @@ public class Monster : MonoBehaviour {
 
 	public void attackFinished() {
 		GetComponent<Animator> ().SetBool ("Attacking", false);
-<<<<<<< HEAD
 		if (nearPlayer) {
-			GameObject.Find ("player").GetComponent<PlayerController> ().hitByMonster (this.gameObject);
+			GameObject.Find ("player").GetComponent<PlayerController> ().HitByMonster (this.gameObject);
 		}
-=======
-		GameObject.Find ("player").GetComponent<PlayerController>().HitByMonster(this.gameObject);
->>>>>>> origin/master
 	}
 
 	void OnTriggerExit(Collider col) {
