942f191a938a8cba7080242efa66d022b1c74f30
Matthew <matthew_palma37@live.ca>
2016-06-03 22:09:53 -0400

    polish

--- Assets/Cinema.cs
+++ Assets/Cinema.cs
@@ -3,9 +3,32 @@ using System.Collections;
 
 public class Cinema : MonoBehaviour {
 
+    public AudioClip one;
+    public AudioClip two;
+    public AudioClip three;
+
 	public void finishedShowing() {
 		Application.LoadLevel ("mattScene");
 	}
+
+    public void playVO(int i)
+    {
+        if (i == 1)
+        {
+            this.GetComponent<AudioSource>().clip = one;
+            this.GetComponent<AudioSource>().Play();
+        }
+        else if (i == 2)
+        {
+            this.GetComponent<AudioSource>().clip = two;
+            this.GetComponent<AudioSource>().Play();
+        }
+        else
+        {
+            this.GetComponent<AudioSource>().clip = three;
+            this.GetComponent<AudioSource>().Play();
+        }
+    }
 
 	// Use this for initialization
 	void Start () {
---
+++ Assets/EndGame.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class EndGame : MonoBehaviour {
+
+    public GameObject lastFirewall;
+    public GameObject endScreen;
+    public GameObject effect;
+    private bool hasEnded = false;
+    private bool hasFX = false;
+
+    void Update()
+    {
+        if (lastFirewall == null && !hasFX)
+        {
+            hasFX = true;
+            effect.SetActive(true);
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.name == "player" && lastFirewall == null && !hasEnded)
+        {
+            hasEnded = true;
+            StartCoroutine(BackToMainMenu());
+        }
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.name == "player" && lastFirewall == null && !hasEnded)
+        {
+            hasEnded = true;
+            StartCoroutine(BackToMainMenu());
+        }
+    }
+
+    IEnumerator BackToMainMenu()
+    {
+        Camera.main.GetComponent<CameraFollow>().follow = this.gameObject;
+        GameObject.Find("SoundManager").GetComponent<SoundManager>().playMenuTheme();
+        endScreen.SetActive(true);
+        foreach (GameObject monster in GameObject.FindGameObjectsWithTag("Monster"))
+        {
+            Destroy(monster.transform.root);
+        }
+        Destroy(GameObject.Find("player"));
+        Destroy(GameObject.Find("pai"));
+        float timer = 6f;
+        while(timer > 0f)
+        {
+            timer -= Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        SceneManager.LoadScene(0);
+    }
+
+}
